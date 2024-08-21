c9fc343ba2eedf9594effd29a845acce3162bb89
Richard Darwin Sy <rs106848@gmail.com>
2024-08-21 12:12:06 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorRed { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello MAUI!";
+            ColorRed = Colors.Red;
+            this.BindingContext = this;
+        }
+        
+        private void OnChangedLabelName(object sender, EventArgs e) 
+        {
+            this.Resources["StaticUsername"] = "Peter Butter";
+        }
+
+        private void OnChangedLabelNameDynamic(object sender, EventArgs e)
+        {
+            this.Resources["DynamicUsername"] = "Its Jover";
+        }
+        private void OnChangedBackgroundColorGreen(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Green;
+        }
+        private void OnChangedBackgroundColorGray(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Gray;
+        }
+        private void OnChangedBackgroundColorBlue(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Blue;
+        }
+
+        private void OnChangedBoxViewColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBoxColor"] = Colors.LightGoldenrodYellow;
+        }
+    }
+
+
+}
